457bb116eb14b21963796b6d1fff0af652e00dd4
Adrian Kennard <a@k.gg>
2020-10-01 07:42:44 +0100

    Positioning tweek

--- qr.c
+++ qr.c
@@ -279,9 +279,9 @@ int main(int argc, const char *argv[])
                if (x < W)
                   break;
             }
-            if (y < W && !(grid[y * W + ox + ow - 1] & QR_TAG_ECC))
+            if (y < W && (grid[y * W + ox + ow - 1] & QR_TAG_DATA) && !(grid[y * W + ox + ow - 1] & QR_TAG_ECC))
                y++;
-            if (y < W && !(grid[y * W + ox + ow - 1] & QR_TAG_ECC))
+            if (y < W && (grid[y * W + ox + ow - 1] & QR_TAG_DATA) && !(grid[y * W + ox + ow - 1] & QR_TAG_ECC))
                y++;
             oy = y - oh;
          }
